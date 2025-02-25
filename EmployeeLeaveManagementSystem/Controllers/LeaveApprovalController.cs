7a8bc65196ad4dea6229c6ff99a213d42173cd04
Pannuru Divyasree <Pannuru.Divyasree@ibm.com>
2025-02-25 18:06:30 +0530

    approve leave 1

--- EmployeeLeaveManagementSystem/Controllers/LeaveApprovalController.cs
+++ EmployeeLeaveManagementSystem/Controllers/LeaveApprovalController.cs
@@ -47,7 +47,7 @@ namespace EmployeeLeaveManagementSystem.Controllers
                     EndDate = x.EndDate,
                     Status = leaveRequest.Status,
                     NoOfLeaves = x.NoOfLeaves,
-                    LeaveType = x.LeaveType,
+                    //LeaveType = x.LeaveType,
                     Reason = x.Reason
                 }).FirstOrDefault();
 
@@ -68,7 +68,7 @@ namespace EmployeeLeaveManagementSystem.Controllers
                     EndDate = x.EndDate,
                     Status = leaveRequest.Status,
                     NoOfLeaves = x.NoOfLeaves,
-                    LeaveType = x.LeaveType,
+                    //LeaveType = x.LeaveType,
                     Reason = x.Reason
                 }).FirstOrDefault();
                 _context.LeaveRequests.Update(getleaverequest);
@@ -76,11 +76,11 @@ namespace EmployeeLeaveManagementSystem.Controllers
                 var updateUserleavebalance = _context.Users.Where(x => x.Id == getleaverequest.UserId).FirstOrDefault();
                 if (updateUserleavebalance != null)
                 {
-                    if(getleaverequest.LeaveType.ToLower() == "sick")
+                    if(getleaverequest.Reason.ToLower() == "sick")
                     {
                         updateUserleavebalance.PendingSickLeaves += getleaverequest.NoOfLeaves;
                     }
-                    else if (getleaverequest.LeaveType.ToLower() == "vacation")
+                    else if (getleaverequest.Reason.ToLower() == "vacation")
                     {
                         updateUserleavebalance.PendingVacationLeaves += getleaverequest.NoOfLeaves;
                     }
